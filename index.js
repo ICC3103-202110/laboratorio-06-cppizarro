f42d8fa38a35b77169294470575a00196ce68fb1
cppizarro <cppizarro@miuandes.cl>
2021-05-21 19:31:13 -0400

    .

--- index.js
+++ index.js
@@ -2,39 +2,3 @@ const {app} = require('./app')
 const {initModel} = require('./model')
 
 app(initModel)
-
-/*
-const inquirer = require("inquirer");
-
-inquirer.prompt([
-    {
-        type: 'confirm',
-        name: 'source',
-        message: 'Left temperature is source? '
-
-    },
-    {
-        type: 'list',
-        message: 'From?',
-        name: 'from',
-        choices: ['Celsius', 'Fahrenheit', 'Kelvin']
-
-    },
-    {
-        type: 'list',
-        message: 'To?',
-        name: 'to',
-        choices: ['Celsius', 'Fahrenheit', 'Kelvin']
-
-    }
-])
-.then(function(answer) {
-    if (answer.source) {
-        console.log('hola')
-    }
-    else {
-        console.log('chao')
-    }
-})
-
-*/
